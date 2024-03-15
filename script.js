56a2130a72544fd05a0430104381fd8d51b3d821
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-03-15 13:26:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function Show_a(){
+    document.getElementById('a').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque perferendis, incidunt quisquam tempora adipiscifacilis. Ad, quaerat, mollitia placeat in nobis, voluptate delectus possimus ratione quasi maiores at temporibus labore."
+}
+function Show_b(){
+    document.getElementById('b').innerHTML = "Doloremque perferendis, incidunt quisquam tempora adipiscifacilis. Ad, quaerat, mollitia placeat in nobis, voluptate delectus possimus ratione quasi maiores at tempo"
+}
+function Show_c(){
+    document.getElementById('c').innerHTML = "Consectetur adipisicing elit. Doloremque perferendis, incidunt quisquam tempora adipiscifacilis. Ad, quaerat, mollitia placeat in nobis, voluptate delectus possimus ratione quasi maiores at temporibus labore."
+}
+function Hide_a(){
+    document.getElementById('a').innerHTML = "";
+}
+function Hide_b(){
+    document.getElementById('b').innerHTML = "";
+}
+function Hide_c(){
+    document.getElementById('c').innerHTML = "";
+}
